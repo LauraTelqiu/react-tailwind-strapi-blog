f5b53ff58cd6bee220aea7466ce9ade0d978f02d
Laura <lauratelqiu@gmail.com>
2023-04-07 14:24:11 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-export default function App() {
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+    </>
+
   )
 }
+
+export default App
