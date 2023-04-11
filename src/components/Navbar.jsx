532a9473e07531522705cc76597a0d3ad253d42e
Laura <lauratelqiu@gmail.com>
2023-04-11 10:41:23 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,36 @@
-import React, { useState } from 'react';
-import { menu, close, logo } from '../assets';
+import React, { useState } from "react";
+import { menu, close, logo } from "../assets";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
-  const handleClick = () => setToggle(!toggle)
-
-
   return (
-    <div className='w-full h-[80px] z-10 bg-white fixed drop-shadow-lg relative'>
-      <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
-      </div>
+    <div className="w-full h-[80px] z-10 bg-white fixed drop-shadow-lg">
+      <div className="flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto">
+        <div className="flex items-center">
+          <img
+            src={logo}
+            alt="logo"
+            className="sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]"
+          />
+        </div>
+        <div className="flex items-center">
+          <ul className="hidden md:flex">
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Platform</li>
+            <li>Pricing</li>
+          </ul>
+        </div>
 
-
-      <div className='flex items-center'>
-        <img src={logo} alt="logo" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]' />
-      </div>
-
-
-      <div className='flex items-center'>
-        <ul className='hidden md:flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Support</li>
-          <li>Platform</li>
-          <li>Pricing</li>
-        </ul>
+        <div className="hidden md:flex sm:mr-10 md:mr-10">
+          <button className="border-none bg-transparent text-black mr-4">
+            Login
+          </button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
       </div>
     </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-  )
-}
-
-export default Navbar
+export default Navbar;
