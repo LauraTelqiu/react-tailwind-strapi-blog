0c78c9e927f16cc704194eba9b1a5d17d471ba39
Laura <lauratelqiu@gmail.com>
2023-04-13 10:24:37 -0400

    blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Blogs from './components/Blogs'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Blogs />
     </>
 
   )
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Blogs from "./Blogs";
+
+
+
+
+
+export {
+  Navbar,
+  Blogs,
+
+}
