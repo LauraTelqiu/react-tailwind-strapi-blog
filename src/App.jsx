b70d399f6d52078fc277798e3856c59717922241
Laura <lauratelqiu@gmail.com>
2023-04-14 11:26:30 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Blogs from './components/Blogs'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Blogs />
+      <Footer />
     </>
 
   )
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -6,7 +6,7 @@ const Blogs = () => {
     <div className='w-full bg-[#f9f9f9] py-[50px]'>
       <div className='max-w-[1240px] mx-auto'>
         <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 ss:grid-cols-1 gap-8 px-4 text-black'>
-          {blogs1.map((blog) => (
+          {blogs1.map((blog) =>
             <div className='bg-white rounded-xl overflow-hidden drop-shadow-md' key={blog.id}>
               <img className='h-56 w-full object-cover' src={blog.coverImg} alt={blog.title} />
               <div className='p-8'>
@@ -16,7 +16,7 @@ const Blogs = () => {
                 </p>
               </div>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div >
